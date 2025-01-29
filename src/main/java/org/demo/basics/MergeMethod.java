dc603e9726e57b239ab630a7f87d1a593051a932
auodumbar <auodumbar@cdac.in>
2025-01-29 17:33:14 +0530

    Basics Completed

--- src/main/java/org/demo/basics/MergeMethod.java
+++ src/main/java/org/demo/basics/MergeMethod.java
@@ -27,20 +27,17 @@ public class MergeMethod {
             //finds an entity instance by id taken from the passed object (either an existing entity instance from the persistence context is retrieved, or a new instance loaded from the database)
             //copies fields from the passed object to this instance
             //returns a newly updated instance
-            //Since the entity doesn’t exist in the database, Hibernate will insert the entity as a new row in the database.
-            Student managedStudent = entityManager.merge(student);
+            Student managedStudent = entityManager.merge(student); //If entity is in transient state i.e. if we comment line 21 - 23 then insert will happen
 
             //Note that the merge method returns an object. It’s the managedStudent object we loaded into the persistence context and updated,
             //not the student object that we passed as an argument. They’re two different objects, and we usually need to discard the student object.
 
-            Student student2 = entityManager.find(Student.class,2702);
-            entityManager.detach(student2);
-
-            student2.setCountry("India");
-
-            entityManager.merge(student2);
-
-
+//            Student student2 = entityManager.find(Student.class,2702);
+//            entityManager.detach(student2);
+//
+//            student2.setCountry("India");
+//            entityManager.merge(student2);
+//
             entityManager.getTransaction().commit();
         }
     }
--- src/main/java/org/demo/basics/UpdateMethod.java
+++ src/main/java/org/demo/basics/UpdateMethod.java
@@ -14,21 +14,23 @@ public class UpdateMethod {
         // Obtain a Hibernate Session
         Session session = PersistenceUtil.getHibernateSession();
 
-        // Begin the transaction
+        //Begin the transaction
         session.getTransaction().begin();
 
         //As with persist and save, the update method is an “original” Hibernate method. Its semantics differ in several key points:
         //it acts upon a passed object (its return type is void). The update method transitions the passed object from a detached to persistent state.
-        //save() and update()  is deprecated since version 6.0
+        //update()  is deprecated since version 6.0
         Student student1 = new Student("Hari", LocalDate.now(), "China");
-        session.save(student1); // Executes INSERT immediately
+        session.save(student1);
         session.evict(student1); // detached from persistence context
 
-        student1.setCountry("Jorden");
-        session.update(student1); //Re-attached to context transient -> persistence
+        student1.setCountry("Jorden"); //modifying object
+
+        //It executes the SQL UPDATE query for PERSISTENT entities.
+        //It throws TransientObjectException if there is no identifier associated (transient entity). if comment line 24 and 25
+        session.update(student1); //Re-attached to context transient -> persistence it will update row
 
         session.getTransaction().commit();
-
         session.close();
 
         Session session2 = PersistenceUtil.getHibernateSession();
@@ -38,10 +40,20 @@ public class UpdateMethod {
 
         //Trying to call update on a transient instance will result in an exception. The following won’t work:
         session2.update(student2); //throws exception
-
         session2.getTransaction().commit();
-
         session2.close();
+
+        Session session3 = PersistenceUtil.getHibernateSession();
+        session3.getTransaction().begin();
+
+        Student student3 = session3.get(Student.class, 3502);
+        student3.setCountry("Paris");
+        session3.update(student3);//it will update as student3 in context
+
+        session3.getTransaction().commit();
+        session3.close();
+
+
     }
 }
 
